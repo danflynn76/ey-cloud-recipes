89c1b546781eb0285b2af37ef7b6622fbaed8d0e
Dan Flynn <danflynn76@gmail.com>
2014-10-20 15:02:46 -0700

    new engine yard chef recipe for building octave from source code

---
+++ cookbooks/octave/recipes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: octave
+# Recipe:: default
+#
+
+pkgs = %w{ sci-libs/cxsparse sci-libs/qrupdate sci-libs/hdf5 sci-libs/arpack sci-mathematics/glpk sci-libs/fftw media-libs/qhull dev-libs/libpcre  }
+
+Chef::Log.info "Installing octave dependencies"
+
+pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+Chef::Log.info "Downloading octave source code"
+
+remote_file "#{Chef::Config[:file_cache_path]}/octave-3.8.1.tar.gz" do
+  source "ftp://ftp.gnu.org/gnu/octave/octave-3.8.1.tar.gz"
+  mode '0644'
+  notifies :run, "bash[install_octave]", :immediately
+end
+
+Chef::Log.info "Compiling and installing ocatve"
+
+bash 'install_octave' do
+  user "root"
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    tar -zxf octave-3.8.1.tar.gz
+    (cd octave-3.8.1/ && ./configure && make && make install)
+  EOH
+  not_if { ::File.exists?("/usr/local/bin/octave") }
+end
